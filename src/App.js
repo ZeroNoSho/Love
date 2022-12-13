92f47b0d56f885e8946477bc02f8aa9c32c68c42
ZeroNoSho <inukuro351@gmail.com>
2022-12-13 20:38:55 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,12 @@ import AnimatedSphere from "./components/AnimatedSphere";
 export default function App() {
   return (
     <div>
-      {/* <Canvas className="canvas">
-        <OrbitControls></OrbitControls>
-        <ambientLight intensity={0.5}></ambientLight>
-        <directionalLight position={[-2, 5, 2]} intensity={1}></directionalLight>
-        <Suspense fallback={null}>
-          <Flower></Flower>
-        </Suspense>
-      </Canvas> */}
       <Canvas className="canvas">
         <OrbitControls></OrbitControls>
         <ambientLight intensity={0.5}></ambientLight>
         <directionalLight position={[-2, 5, 2]} intensity={1}></directionalLight>
         <Suspense fallback={null}>
-          <AnimatedSphere></AnimatedSphere>
+          <Flower></Flower>
         </Suspense>
       </Canvas>
     </div>
